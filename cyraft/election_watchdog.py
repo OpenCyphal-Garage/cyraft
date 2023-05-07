6477a9c982333e6fb7f5681e579d6c7c4087a499
Maksim Drachov <maksim.drachov@outlook.com>
2023-05-07 14:18:17 +0300

    election timeout without drift

--- cyraft/election_watchdog.py
+++ cyraft/election_watchdog.py
@@ -8,36 +8,32 @@ _logger = logging.getLogger(__name__)
 
 class ElectionWatchdog:
     def __init__(self) -> None:
-        self.closing = False
         self.election_timeout = ELECTION_TIMEOUT
-        self.reset_election_timeout = False
-        self.election_timeout_reached = False
+        self._next_election_timeout: float
+        self._election_timer: asyncio.TimerHandle
+        self.election_timeout_reached: bool = False
 
-    async def watchdog_timer(self, timeout):
-        while not self.closing:
-            await asyncio.sleep(timeout)
-            # Check if I need to start election (if the timer has been reset in the meantime)
-            if self.reset_election_timeout == True:
-                self.reset_election_timeout = False
-            else:
-                self.on_election_timeout()
-
-    def kick_election_timeout(self) -> None:
-        self.reset_election_timeout = True
-        _logger.debug("Kicking the election timeout watchdog")
+    def reset_election_timeout(self) -> None:
+        _logger.debug("Reseting the election timer")
+        loop = asyncio.get_event_loop()
+        self._next_election_timeout = loop.time() + self.election_timeout
+        self._election_timer.cancel()
+        self._election_timer = loop.call_at(self._next_election_timeout, self.on_election_timeout)
 
     def on_election_timeout(self) -> None:
         _logger.debug("Election timeout reached!")
         self.election_timeout_reached = True
+        self.reset_election_timeout()
 
     async def run(self) -> None:
         _logger.debug("Starting election watchdog")
-        election_watchdog = asyncio.create_task(self.watchdog_timer(self.election_timeout))
-        while not self.closing:
-            await asyncio.sleep(1)
+        loop = asyncio.get_event_loop()
+        self._next_election_timeout = loop.time() + self.election_timeout
+        self._election_timer = loop.call_at(self._next_election_timeout, self.on_election_timeout)
 
+    def close(self) -> None:
         _logger.debug("Closing election watchdog")
-        election_watchdog.cancel()
+        self._election_timer.cancel()
 
 
 # ----------------------------------------  TESTS GO BELOW THIS LINE  ----------------------------------------
@@ -49,18 +45,20 @@ async def _unittest_election_watchdog() -> None:
 
     await asyncio.sleep(ELECTION_TIMEOUT - 1)  # Election timeout should not be reached here
 
-    watchdog_node.kick_election_timeout()  # Reset election timeout
+    watchdog_node.reset_election_timeout()  # Reset election timeout
 
     await asyncio.sleep(1)  # Election timeout should be reached here, but it should be reset above
 
     _logger.debug("==== Election timeout should not be reached above this line ====")
-    assert watchdog_node.election_timeout_reached == False
+    assert watchdog_node.election_timeout_reached is False
 
     await asyncio.sleep(ELECTION_TIMEOUT)
 
     _logger.debug("==== Election timeout should be reached above this line ====")
-    assert watchdog_node.election_timeout_reached == True
+    assert watchdog_node.election_timeout_reached is True
 
     # closing
-    watchdog_node.closing = True
+    watchdog_node.close()
     await asyncio.sleep(1)
+
+    assert False
