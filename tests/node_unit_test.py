2d029aae64d578b59f4aa3006d3ddb175f8b4e03
Maksim Drachov <maksim.drachov@outlook.com>
2023-05-10 14:12:06 +0300

    _unittest_raft_node_append_entries_rpc

--- tests/node_unit_test.py
+++ tests/node_unit_test.py
@@ -8,6 +8,7 @@ import time
 
 import pycyphal
 import sirius_cyber_corp
+import uavcan
 
 # Add parent directory to Python path
 sys.path.append(os.path.abspath("/Users/maksimdrachov/cyraft"))  # Q: how to make it relative?``
@@ -376,3 +377,403 @@ async def _unittest_raft_node_start_election() -> None:
     raft_node_1.close()
     raft_node_2.close()
     await asyncio.sleep(1)
+
+
+async def _unittest_raft_node_append_entries_rpc() -> None:
+    """
+    Test the _serve_append_entries method
+
+     Step 1: Append 3 log entries
+       ____________ ____________ ____________ ____________
+      | 0          | 1          | 2          | 3          |     Log index
+      | 0          | 4          | 5          | 6          |     Log term
+      | empty <= 0 | top_1 <= 7 | top_2 <= 8 | top_3 <= 9 |     Name <= value
+      |____________|____________|____________|____________|
+
+     Step 2: Replace log entry 3 with a new entry
+       ____________
+      | 3          |     Log index
+      | 7          |     Log term
+      | top_3 <= 10|     Name <= value
+      |____________|
+
+     Step 3: Replace log entries 2 and 3 with new entries
+       ____________ ____________
+      | 2          | 3          |     Log index
+      | 8          | 9          |     Log term
+      | top_2 <= 11| top_3 <= 12|     Name <= value
+      |____________|____________|
+
+     Step 4: Add an already existing log entry
+       ____________
+      | 3          |     Log index
+      | 9          |     Log term
+      | top_3 <= 12|     Name <= value
+      |____________|
+
+     Step 5: Add an additional log entry
+       ____________
+      | 4          |     Log index
+      | 10         |     Log term
+      | top_4 <= 13|     Name <= value
+      |____________|
+
+     Result:
+       ____________ ____________ ____________ ____________ ____________
+      | 0          | 1          | 2          | 3          | 4          |     Log index
+      | 0          | 4          | 8          | 9          | 10         |     Log term
+      | empty <= 0 | top_1 <= 7 | top_2 <= 10| top_3 <= 11| top_4 <= 13|     Name <= value
+      |____________|____________|____________|____________|____________|
+
+     Step 6: Try to append old log entry (term < currentTerm)
+       ____________
+      | 4          |     Log index
+      | 9          |     Log term
+      | top_4 <= 14|     Name <= value
+      |____________|
+
+     Step 7: Try to append valid log entry, however entry at prev_log_index term does not match
+       ____________
+      | 4          |     Log index
+      | 11         |     Log term
+      | top_4 <= 15|     Name <= value
+      |____________|
+
+
+      Every step check:
+        - self._term
+        - self._voted_for
+        - self._log
+        - self._commit_index
+    """
+
+    os.environ["UAVCAN__NODE__ID"] = "41"
+    raft_node = RaftNode()
+    raft_node._voted_for = 42
+
+    _logger.info("================== TEST 1: append 3 log entries ==================")
+    new_entries = [
+        sirius_cyber_corp.LogEntry_1(
+            term=4,
+            entry=sirius_cyber_corp.Entry_1(
+                name=uavcan.primitive.String_1(value="top_1"),
+                value=7,
+            ),
+        ),
+        sirius_cyber_corp.LogEntry_1(
+            term=5,
+            entry=sirius_cyber_corp.Entry_1(
+                name=uavcan.primitive.String_1(value="top_2"),
+                value=8,
+            ),
+        ),
+        sirius_cyber_corp.LogEntry_1(
+            term=6,
+            entry=sirius_cyber_corp.Entry_1(
+                name=uavcan.primitive.String_1(value="top_3"),
+                value=9,
+            ),
+        ),
+    ]
+
+    for index, new_entry in enumerate(new_entries):
+        request = sirius_cyber_corp.AppendEntries_1.Request(
+            term=6,
+            prev_log_index=index,  # prev_log_index: 0, 1, 2
+            prev_log_term=raft_node._log[index].term,
+            log_entry=new_entry,
+        )
+        metadata = pycyphal.presentation.ServiceRequestMetadata(
+            client_node_id=42,
+            timestamp=time.time(),
+            priority=0,
+            transfer_id=0,
+        )
+        response = await raft_node._serve_append_entries(request, metadata)
+        assert response.success == True
+
+    assert raft_node._term == 6
+    assert raft_node._voted_for == 42
+
+    assert len(raft_node._log) == 1 + 3
+    assert raft_node._log[0].term == 0
+    assert raft_node._log[0].entry.name.value.tobytes().decode("utf-8") == ""  # index zero entry is empty
+    assert raft_node._log[0].entry.value == 0
+    assert raft_node._log[1].term == 4
+    assert raft_node._log[1].entry.name.value.tobytes().decode("utf-8") == "top_1"
+    assert raft_node._log[1].entry.value == 7
+    assert raft_node._log[2].term == 5
+    assert raft_node._log[2].entry.name.value.tobytes().decode("utf-8") == "top_2"
+    assert raft_node._log[2].entry.value == 8
+    assert raft_node._log[3].term == 6
+    assert raft_node._log[3].entry.name.value.tobytes().decode("utf-8") == "top_3"
+    assert raft_node._log[3].entry.value == 9
+    assert raft_node._commit_index == 3
+
+    # Once https://github.com/OpenCyphal/pycyphal/issues/297 is fixed, we can do this (instead of the above):
+    # assert raft_node.log[0] == index_zero_entry
+    # assert raft_node.log[1] == new_entries[0]
+    # assert raft_node.log[2] == new_entries[1]
+    # assert raft_node.log[3] == new_entries[2]
+
+    _logger.info("================== TEST 2: Replace log entry 3 with a new entry ==================")
+
+    new_entry = sirius_cyber_corp.LogEntry_1(
+        term=7,
+        entry=sirius_cyber_corp.Entry_1(
+            name=uavcan.primitive.String_1(value="top_3"),
+            value=10,
+        ),
+    )
+    request = sirius_cyber_corp.AppendEntries_1.Request(
+        term=7,
+        prev_log_index=2,  # index of top_2
+        prev_log_term=raft_node._log[2].term,
+        log_entry=new_entry,
+    )
+    metadata = pycyphal.presentation.ServiceRequestMetadata(
+        client_node_id=42,
+        timestamp=time.time(),
+        priority=0,
+        transfer_id=0,
+    )
+    response = await raft_node._serve_append_entries(request, metadata)
+    assert response.success == True
+
+    assert raft_node._term == 7
+    assert raft_node._voted_for == 42
+
+    assert len(raft_node._log) == 1 + 3
+    assert raft_node._log[0].term == 0
+    assert raft_node._log[0].entry.name.value.tobytes().decode("utf-8") == ""  # index zero entry is empty
+    assert raft_node._log[0].entry.value == 0
+
+    assert raft_node._log[1].term == 4
+    assert raft_node._log[1].entry.name.value.tobytes().decode("utf-8") == "top_1"
+    assert raft_node._log[1].entry.value == 7
+    assert raft_node._log[2].term == 5
+    assert raft_node._log[2].entry.name.value.tobytes().decode("utf-8") == "top_2"
+    assert raft_node._log[2].entry.value == 8
+    assert raft_node._log[3].term == 7
+    assert raft_node._log[3].entry.name.value.tobytes().decode("utf-8") == "top_3"
+    assert raft_node._log[3].entry.value == 10
+    assert raft_node._commit_index == 3
+
+    _logger.info("================== TEST 3: Replace log entries 2 and 3 with new entries ==================")
+
+    new_entries = [
+        sirius_cyber_corp.LogEntry_1(
+            term=8,
+            entry=sirius_cyber_corp.Entry_1(
+                name=uavcan.primitive.String_1(value="top_2"),
+                value=11,
+            ),
+        ),
+        sirius_cyber_corp.LogEntry_1(
+            term=9,
+            entry=sirius_cyber_corp.Entry_1(
+                name=uavcan.primitive.String_1(value="top_3"),
+                value=12,
+            ),
+        ),
+    ]
+
+    for index, new_entry in enumerate(new_entries):
+        request = sirius_cyber_corp.AppendEntries_1.Request(
+            term=9,
+            prev_log_index=index + 1,  # index: 1, 2
+            prev_log_term=raft_node._log[index + 1].term,
+            log_entry=new_entry,
+        )
+        metadata = pycyphal.presentation.ServiceRequestMetadata(
+            client_node_id=42,
+            timestamp=time.time(),
+            priority=0,
+            transfer_id=0,
+        )
+        response = await raft_node._serve_append_entries(request, metadata)
+        assert response.success == True
+
+    assert raft_node._term == 9
+    assert raft_node._voted_for == 42
+
+    assert len(raft_node._log) == 1 + 3
+    assert raft_node._log[0].term == 0
+    assert raft_node._log[0].entry.value == 0
+    assert raft_node._log[1].term == 4
+    assert raft_node._log[1].entry.name.value.tobytes().decode("utf-8") == "top_1"
+    assert raft_node._log[1].entry.value == 7
+    assert raft_node._log[2].term == 8
+    assert raft_node._log[2].entry.name.value.tobytes().decode("utf-8") == "top_2"
+    assert raft_node._log[2].entry.value == 11
+    assert raft_node._log[3].term == 9
+    assert raft_node._log[3].entry.name.value.tobytes().decode("utf-8") == "top_3"
+    assert raft_node._log[3].entry.value == 12
+
+    _logger.info("================== TEST 4: Add an already existing log entry ==================")
+
+    new_entry = sirius_cyber_corp.LogEntry_1(
+        term=9,
+        entry=sirius_cyber_corp.Entry_1(
+            name=uavcan.primitive.String_1(value="top_3"),
+            value=12,
+        ),
+    )
+    request = sirius_cyber_corp.AppendEntries_1.Request(
+        term=9,
+        prev_log_index=2,  # index of top_2
+        prev_log_term=raft_node._log[2].term,
+        log_entry=new_entry,
+    )
+    metadata = pycyphal.presentation.ServiceRequestMetadata(
+        client_node_id=42,
+        timestamp=time.time(),
+        priority=0,
+        transfer_id=0,
+    )
+
+    response = await raft_node._serve_append_entries(request, metadata)
+    assert response.success == True
+
+    assert raft_node._term == 9
+    assert raft_node._voted_for == 42
+
+    assert len(raft_node._log) == 1 + 3
+    assert raft_node._log[0].term == 0
+    assert raft_node._log[0].entry.value == 0
+    assert raft_node._log[1].term == 4
+    assert raft_node._log[1].entry.name.value.tobytes().decode("utf-8") == "top_1"
+    assert raft_node._log[1].entry.value == 7
+    assert raft_node._log[2].term == 8
+    assert raft_node._log[2].entry.name.value.tobytes().decode("utf-8") == "top_2"
+    assert raft_node._log[2].entry.value == 11
+    assert raft_node._log[3].term == 9
+    assert raft_node._log[3].entry.name.value.tobytes().decode("utf-8") == "top_3"
+    assert raft_node._log[3].entry.value == 12
+
+    _logger.info("================== TEST 5: Add an additional log entry ==================")
+
+    new_entry = sirius_cyber_corp.LogEntry_1(
+        term=10,
+        entry=sirius_cyber_corp.Entry_1(
+            name=uavcan.primitive.String_1(value="top_4"),
+            value=13,
+        ),
+    )
+    request = sirius_cyber_corp.AppendEntries_1.Request(
+        term=10,
+        prev_log_index=3,  # index of top_3
+        prev_log_term=raft_node._log[3].term,
+        log_entry=new_entry,
+    )
+    metadata = pycyphal.presentation.ServiceRequestMetadata(
+        client_node_id=42,
+        timestamp=time.time(),
+        priority=0,
+        transfer_id=0,
+    )
+    response = await raft_node._serve_append_entries(request, metadata)
+    assert response.success == True
+
+    assert raft_node._term == 10
+    assert raft_node._voted_for == 42
+
+    assert len(raft_node._log) == 1 + 4
+    assert raft_node._log[0].term == 0
+    assert raft_node._log[0].entry.value == 0
+    assert raft_node._log[1].term == 4
+    assert raft_node._log[1].entry.name.value.tobytes().decode("utf-8") == "top_1"
+    assert raft_node._log[1].entry.value == 7
+    assert raft_node._log[2].term == 8
+    assert raft_node._log[2].entry.name.value.tobytes().decode("utf-8") == "top_2"
+    assert raft_node._log[2].entry.value == 11
+    assert raft_node._log[3].term == 9
+    assert raft_node._log[3].entry.name.value.tobytes().decode("utf-8") == "top_3"
+    assert raft_node._log[3].entry.value == 12
+    assert raft_node._log[4].term == 10
+    assert raft_node._log[4].entry.name.value.tobytes().decode("utf-8") == "top_4"
+    assert raft_node._log[4].entry.value == 13
+
+    _logger.info("================== TEST 6: Try to append old log entry (term < currentTerm) ==================")
+
+    new_entry = sirius_cyber_corp.LogEntry_1(
+        term=9,
+        entry=sirius_cyber_corp.Entry_1(
+            name=uavcan.primitive.String_1(value="top_4"),
+            value=14,
+        ),
+    )
+    request = sirius_cyber_corp.AppendEntries_1.Request(
+        term=9,
+        prev_log_index=3,
+        prev_log_term=raft_node._log[3].term,
+        log_entry=new_entry,
+    )
+    metadata = pycyphal.presentation.ServiceRequestMetadata(
+        client_node_id=42,
+        timestamp=time.time(),
+        priority=0,
+        transfer_id=0,
+    )
+    response = await raft_node._serve_append_entries(request, metadata)
+    assert response.success == False
+
+    assert raft_node._term == 10
+    assert raft_node._voted_for == 42
+
+    assert len(raft_node._log) == 1 + 4
+    assert raft_node._log[0].term == 0
+    assert raft_node._log[0].entry.value == 0
+    assert raft_node._log[1].term == 4
+    assert raft_node._log[1].entry.name.value.tobytes().decode("utf-8") == "top_1"
+    assert raft_node._log[1].entry.value == 7
+    assert raft_node._log[2].term == 8
+    assert raft_node._log[2].entry.name.value.tobytes().decode("utf-8") == "top_2"
+    assert raft_node._log[2].entry.value == 11
+    assert raft_node._log[3].term == 9
+    assert raft_node._log[3].entry.name.value.tobytes().decode("utf-8") == "top_3"
+    assert raft_node._log[3].entry.value == 12
+    assert raft_node._log[4].term == 10
+    assert raft_node._log[4].entry.name.value.tobytes().decode("utf-8") == "top_4"
+    assert raft_node._log[4].entry.value == 13
+
+    _logger.info(
+        "================== TEST 7: Try to append valid log entry, however prev_log_index term does not match =================="
+    )
+
+    new_entry = sirius_cyber_corp.LogEntry_1(
+        term=11,
+        entry=sirius_cyber_corp.Entry_1(
+            name=uavcan.primitive.String_1(value="top_4"),
+            value=15,
+        ),
+    )
+    request = sirius_cyber_corp.AppendEntries_1.Request(
+        term=11,
+        prev_log_index=4,
+        prev_log_term=raft_node._log[4].term - 1,  # term mismatch
+        log_entry=new_entry,
+    )
+    metadata = pycyphal.presentation.ServiceRequestMetadata(
+        client_node_id=42,
+        timestamp=time.time(),
+        priority=0,
+        transfer_id=0,
+    )
+    response = await raft_node._serve_append_entries(request, metadata)
+    assert response.success == False
+
+    assert raft_node._term == 10
+    assert raft_node._voted_for == 42
+
+    assert len(raft_node._log) == 1 + 4
+    assert raft_node._log[0].term == 0
+    assert raft_node._log[0].entry.value == 0
+    assert raft_node._log[1].term == 4
+    assert raft_node._log[1].entry.value == 7
+    assert raft_node._log[2].term == 8
+    assert raft_node._log[2].entry.value == 11
+    assert raft_node._log[3].term == 9
+    assert raft_node._log[3].entry.value == 12
+    assert raft_node._log[4].term == 10
+    assert raft_node._log[4].entry.value == 13
